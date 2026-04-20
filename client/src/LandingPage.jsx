8c3c5538001104c2f6d953c5cd4373e6198f58cc
CPE3C-buri-josefkarol <josefkaroll.buri@gmail.com>
2026-04-20 15:39:54 +0800

    Implementation of URL-Driven Authentication System

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -1,21 +1,12 @@
-<<<<<<< Updated upstream
+import { useSearchParams } from "react-router-dom";
 import AboutSection from "./components/LandingPage_comp/AboutSection"
 import FooterSection from "./components/LandingPage_comp/FooterSection"
 import HeroSection from "./components/LandingPage_comp/HeroSection"
 import HowItWorks from "./components/LandingPage_comp/HowItWorks"
 import Navbar from "./components/LandingPage_comp/Navbar"
 import SampleRecipe from "./components/LandingPage_comp/SampleRecipe"
-=======
-import { useSearchParams } from "react-router-dom";
-import AboutSection from "./components/AboutSection"
-import FooterSection from "./components/FooterSection"
-import HeroSection from "./components/HeroSection"
-import HowItWorks from "./components/HowItWorks"
-import Navbar from "./components/Navbar"
-import SampleRecipe from "./components/SampleRecipe"
-import Signup from "./components/Signup"
-import Login from "./components/Login"
->>>>>>> Stashed changes
+import Signup from "./components/LandingPage_comp/Signup"
+import Login from "./components/LandingPage_comp/Login"
 
 const LandingPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,29 +1,13 @@
-import { StrictMode } from 'react' 
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< Updated upstream
-import { BrowserRouter, Routes, Route } from 'react-router-dom' 
-=======
 import { BrowserRouter } from 'react-router-dom'
->>>>>>> Stashed changes
 import './index.css'
 import LandingPage from './LandingPage.jsx'
-import DashboardPage from './DashboardPage.jsx'  
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<<<<<<< Updated upstream
-    <BrowserRouter> 
-      <Routes>       
-        <Route path="/" element={<LandingPage />} />           
-        <Route path="/dashboard" element={<DashboardPage />} /> 
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
-=======
-   <BrowserRouter>
+    <BrowserRouter>
       <LandingPage />
     </BrowserRouter>
   </StrictMode>
 )
->>>>>>> Stashed changes
