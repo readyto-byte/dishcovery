961e1a25aee140d91fbe2b0aeed7e507d80025d7
josefkarollburi <josefkaroll.buri@gmail.com>
2026-04-23 14:40:35 +0800

    Update MealPlanPage.jsx

--- client/src/components/Dashboard/MealPlanPage.jsx
+++ client/src/components/Dashboard/MealPlanPage.jsx
@@ -760,7 +760,7 @@ const MealPlanPage = ({ onViewRecipe, activeProfile }) => {
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div>
                   <label className="block text-sm font-semibold text-gray-700 mb-1">Age</label>
-                  <input type="number" name="age" value={formData.age} onChange={handleFormChange} placeholder="e.g., 25" className="w-full border border-gray-200 rounded-lg px-4 py-2.5 focus:border-[#587A34] focus:ring-1 focus:ring-[#587A34] transition" />
+                  <input type="number" name="age" value={formData.age} onChange={handleFormChange} placeholder="e.g., 25" min='10' className="w-full border border-gray-200 rounded-lg px-4 py-2.5 focus:border-[#587A34] focus:ring-1 focus:ring-[#587A34] transition" />
                 </div>
                 <div>
                   <label className="block text-sm font-semibold text-gray-700 mb-1">Height</label>
